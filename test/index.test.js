34850235337b1dde33bb3792e7c127f1f061a1a6
Zach Schneider <zach@schneider.dev>
2020-07-09 10:57:03 -0500

    Upgrade linting & testing

--- test/index.test.js
+++ test/index.test.js
@@ -1,34 +1,31 @@
 /* eslint-env jest */
 
-import React from 'react';
-import { mount } from 'enzyme';
-import toJson from 'enzyme-to-json';
-import ReactBootstrapMaskedInput from '../src';
+import React from "react";
+import { render } from "@testing-library/react";
+import ReactBootstrapMaskedInput from "../src";
 
-describe('Component', () => {
-  it('renders correctly with basic props', () => {
-    const wrapper = mount(
-      <ReactBootstrapMaskedInput
-        mask='111-111-1111'
-      />
+describe("Component", () => {
+  it("renders correctly with basic props", () => {
+    const { container } = render(
+      <ReactBootstrapMaskedInput mask="111-111-1111" />
     );
 
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(container).toMatchSnapshot();
   });
 
-  it('passes complex props through to FormControl and MaskedInput', () => {
-    const wrapper = mount(
+  it("passes complex props through to FormControl and MaskedInput", () => {
+    const { container } = render(
       <ReactBootstrapMaskedInput
-        bsClass='my-form-control'
-        bsSize='large'
-        id='my-id'
-        inputRef={ref => ref}
-        mask='111-111-1111'
-        placeholder='555-555-5555'
+        bsClass="my-form-control"
+        bsSize="large"
+        id="my-id"
+        inputRef={(ref) => ref}
+        mask="111-111-1111"
+        placeholder="555-555-5555"
         size={10}
       />
     );
 
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(container).toMatchSnapshot();
   });
 });
